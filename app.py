4a1fbee48e0fa2b11cee79b397450761f5e0b4e8
sieuanhta <trinhhaitienemail@gmail.com>
2025-11-17 23:05:02 +0700

    login, submit url, change some line in login.html and push_url.html

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from controllers.main_controller import main_bp
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="views/templates",  # <─ nơi chứa các file .html
+    static_folder="views/static",      # <─ nơi chứa css, images
+)
+app.secret_key = "your-secret-key"   # BẮT BUỘC: session cần secret_key
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.register_blueprint(main_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)

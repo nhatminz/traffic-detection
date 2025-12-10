41612fc443b392db1dd427b90d1f47556c51a465
pham nhat minh <phamnhatminh2k5@gmail.com>
2025-12-10 10:57:40 +0700

    render video

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
 from controllers.main_controller import main_bp
 from controllers.video_controller import video_bp
+
+
+load_dotenv()
 
 app = Flask(
     __name__,
     template_folder="views/templates",  # <─ nơi chứa các file .html
     static_folder="views/static",      # <─ nơi chứa css, images
 )
-app.secret_key = "your-secret-key"   # BẮT BUỘC: session cần secret_key
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')   # BẮT BUỘC: session cần secret_key
+
+from models.decryption import decrypt_file  
+decrypt_file()  
+
+from models.sheets import get_service_account_file
+SERVICE_ACCOUNT_FILE = get_service_account_file()
 
 app.register_blueprint(main_bp)
 app.register_blueprint(video_bp)
